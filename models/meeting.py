b09ffa9f852ba789325110a9f0a6dfb97c5b5f64
Ilya Zhalnin <mr.ilay333@gmail.com>
2021-05-15 14:14:29 +0300

    Переименовывает поля и добавляет дефолтные значения

--- models/meeting.py
+++ models/meeting.py
@@ -1,17 +1,18 @@
 import datetime
 
-from peewee import IntegerField, TextField, DateTimeField, ForeignKeyField
+from peewee import IntegerField, TextField, DateTimeField, ForeignKeyField, BooleanField
 
 from models.base_model import BaseModel
 from models.user import User
 
 
 class Meeting(BaseModel):
-    user_id_1: int = ForeignKeyField(User)
-    user_id_2: int = ForeignKeyField(User)
-    rating_1: int = IntegerField()
-    rating_2: int = IntegerField()
-    feedback_1: str = TextField()
-    feedback_2: str = TextField()
-    meeting_time: datetime.datetime = DateTimeField()
-    status: int = IntegerField()
+    initiator_id: int = ForeignKeyField(User)
+    companion_id: int = ForeignKeyField(User)
+    initiator_rate: int = IntegerField(null=True)
+    companion_rate: int = IntegerField(null=True)
+    initiator_feedback: str = TextField(null=True)
+    companion_feedback: str = TextField(null=True)
+    meeting_time: datetime.datetime = DateTimeField(null=True)
+    status: int = IntegerField(default=0)
+    is_ready: bool = BooleanField(default=False)
