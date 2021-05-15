8830297473ef0f5764427b52e03881014a1cb0d5
Denis <ddiveev99@gmail.com>
2021-05-15 11:49:56 +0300

    добавляет вьюшки для регистрации и поиска встречи

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import telebot
 from telebot.types import Message
 
 from keyboards.start import StartKeyboard
+from keyboards.viewing import ViewingKeyboard
 from settings import TOKEN
+from filters import join_name, join_about, viewing_next, viewing_good, join_successful
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -12,6 +14,46 @@ def start(message: Message):
     bot.send_message(message.chat.id,
                      "Привет, я бот Random Coffee.",
                      reply_markup=StartKeyboard.build())
+
+
+@bot.message_handler(regexp=StartKeyboard.join)
+@bot.message_handler(func=join_name.filter_by_state)
+def join_name(message: Message):
+    bot.send_message(message.chat.id,
+                     "Введите своё имя.")
+
+
+@bot.message_handler(func=join_about.filter_by_state)
+def join_about(message: Message):
+    bot.send_message(message.chat.id,
+                     "Расскажите немного о себе.")
+
+
+@bot.message_handler(func=join_successful.filter_by_state)
+def join_successful(message: Message):
+    bot.send_message(message.chat.id,
+                     "Вы зарегистрировались")
+
+    # TODO: добавить  вывод предложения с кем пообщаться
+    bot.send_message(message.chat.id,
+                     "lorem",
+                     reply_markup=ViewingKeyboard.build())
+
+
+@bot.message_handler(regexp=ViewingKeyboard.next)
+@bot.message_handler(func=viewing_next.filter_by_state)
+def viewing_next(message: Message):
+    bot.send_message(message.chat.id,
+                     "lorem",
+                     reply_markup=ViewingKeyboard.build())
+
+
+@bot.message_handler(regexp=ViewingKeyboard.next)
+@bot.message_handler(func=viewing_good.filter_by_state)
+def viewing_good(message: Message):
+    bot.send_message(message.chat.id,
+                     "lorem",
+                     reply_markup=ViewingKeyboard.build())
 
 
 if __name__ == '__main__':
